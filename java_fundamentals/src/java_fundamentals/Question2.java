11650df0bf021f2af92250252401b6139b16e811
Sikindar <shaiksikindar5560@gmail.com>
2025-07-04 16:31:55 +0530

    04/07/2025 java_fundamentals

---
+++ java_fundamentals/src/java_fundamentals/Question2.java
@@ -0,0 +1,9 @@
+package java_fundamentals;
+import java.util.*;
+public class Question2 {
+   public static void main(String[]args) {
+	  Scanner sc=new Scanner(System.in);
+	  String s1=sc.next();
+	  System.out.println("Welcome "+s1);
+   }
+}
